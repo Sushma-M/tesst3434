b200f09632ed23493f559bca480eaf66a60e20f0
root <root@e5058a25df0e>
2017-05-08 11:48:57 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "tesst3434",
+  "homePage" : "Main",
+  "name" : "tesst3434",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
