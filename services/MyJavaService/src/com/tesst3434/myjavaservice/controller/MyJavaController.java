e683b890289d60d10afb40b7aafcfe4c004f51c8
Sushma Wm <sushma.maddirala@wavemaker.com>
2017-05-09 04:55:09 +0000

    qwewqewe Files modified - [MyJavaService]

--- services/MyJavaService/src/com/tesst3434/myjavaservice/controller/MyJavaController.java
+++ services/MyJavaService/src/com/tesst3434/myjavaservice/controller/MyJavaController.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wavemaker.tools.api.core.annotations.WMAccessVisibility;
+import com.wavemaker.tools.api.core.models.AccessSpecifier;
 
 @RestController
 @RequestMapping(value = "/myJava")
@@ -16,6 +21,8 @@ public class MyJavaController {
     private MyJavaService myJavaService;
 
     @RequestMapping(value = "/sampleJavaOperation", produces = "application/json", method = RequestMethod.GET)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @ApiOperation(value = "")
     public String sampleJavaOperation(@RequestParam(value = "name", required = false) String name, HttpServletRequest request) {
         return myJavaService.sampleJavaOperation(name, request);
     }
